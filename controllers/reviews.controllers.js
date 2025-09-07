3a40d9132aa9c6d6b86549d5bcd208dd1e5031ca
lorenzoalig <lorenzoalig@gmail.com>
2025-09-07 15:23:06 -0300

    Finalized CRUD for reviews and enhanced movie controllers

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -12,6 +12,9 @@ const getReviews = async (req, res) => {
         if(reviews.length == 0) return res.status(200).json({message: 'There are no reviews for this movie'});
         res.status(200).json(reviews);
     } catch (error) {
+        if(error.name === 'CastError') {
+            return res.status(404).json({message: 'Invalid ID'});
+        }
         res.status(500).json({message: error.message});
     }
 }
@@ -25,9 +28,12 @@ const createReview = async (req, res) => {
         movie.reviews.push(req.body);
         await movie.save();
         movie = await Movie.findById(id);
-        res.status(200).json(movie.reviews);
+        res.status(201).json(movie.reviews[movie.reviews.length - 1]);
     } catch (error) {
-        if (error.name === "ValidationError") {
+        if(error.name === 'CastError') {
+            return res.status(404).json({message: 'Invalid ID'});
+        }
+        if (error.name === 'ValidationError') {
             return res.status(400).json({ message: error.message });
         }
         res.status(500).json({ message: error.message });
@@ -45,10 +51,13 @@ const updateReview = async (req, res) => {
         if(!movieReview) return res.status(404).json({message: 'Review not found'});
         const reviewIndex = movie.reviews.indexOf(movieReview);
         movie.reviews[reviewIndex] = req.body;
-        movie = await movie.save();
+        await movie.save();
         res.status(200).json(movie.reviews[reviewIndex]);
     } catch (error) {
-         if (error.name === "ValidationError") {
+        if(error.name === 'CastError') {
+            return res.status(404).json({message: 'Invalid ID'});
+        }
+         if (error.name === 'ValidationError') {
             return res.status(400).json({ message: error.message });
         }
         res.status(500).json({message: error.message});
@@ -61,20 +70,24 @@ const deleteReview = async (req, res) => {
         let movie = await Movie.findById(id);
 
         if(!movie) return res.status(404).json({message: 'Movie not found'});
-        const reviewIndex = movie.reviews.findIndex(r => r._id.equals(req.body._id));
+        const reviewIndex = movie.reviews.findIndex(r => r._id.toString() === req.body._id);
 
         if(reviewIndex < 0) return res.status(404).json({message: 'Review not found'});
-        movie.reviews[reviewIndex].remove();
+        movie.reviews.splice(reviewIndex, 1);
         movie = await movie.save();
         res.status(200).json({message: 'Review deleted successfully'});
     } catch (error) {
-        if (error.name === "ValidationError") {
+        if(error.name === 'CastError') {
+            return res.status(404).json({message: 'Invalid ID'});
+        }
+        if (error.name === 'ValidationError') {
             return res.status(400).json({message: error.message});
         }
         res.status(500).json({message: error.message});
     }
 }
 
+// Exports controllers
 module.exports = {
     getReviews,
     createReview,
